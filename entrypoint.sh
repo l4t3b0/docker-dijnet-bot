2df1fdad5a1ef90a780d72e15fd0196d974bf8eb
root <l4t3b0@gmail.com>
2020-10-29 10:23:26 +0000

    initial release

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 set -e
 
 # Announce version
-echo "INFO: Running $(rclone --version | head -n 1)"
+echo "INFO: Running ${DIJNET_VERSION}"
 
-if [[ ! ${RCLONE_CMD} =~ (copy|move|sync) ]]; then
-  echo "WARNING: rclone command '${RCLONE_CMD}' is not supported by this container, please use sync/copy/move. Stopping."
+if [ -z ${DIJNET_USER} ]; then
+  echo "ERROR: No DIJNET_USER defined. Stopping."
   exit 1
 elif [ -z "${PGID}" -a ! -z "${PUID}" ] || [ -z "${PUID}" -a ! -z "${PGID}" ]; then
   echo "WARNING: Must supply both PUID and PGID or neither. Stopping."
@@ -53,53 +53,48 @@ rm -f /tmp/sync.pid
 
 # Check for source and destination ; launch config if missing
 
-if [ -z "${DIJNET_USER}" ]; then
-  echo "INFO: No DIJNET_USER defined. Stopping"
+if [ -z "${SYNC_ON_STARTUP}" ]
+then
+  echo "INFO: Add SYNC_ON_STARTUP=true to perform a sync upon boot"
+else
+  su "$USER" -c /usr/bin/dijnet-bot-sync.sh
+fi
+
+# Re-write cron shortcut
+case "$(echo "$CRON" | tr '[:lower:]' '[:upper:]')" in
+  *@YEARLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 1 *" && CRONS="0 0 1 1 *";;
+  *@ANNUALLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 1 *" && CRONS="0 0 1 1 *";;
+  *@MONTHLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 * *" && CRONS="0 0 1 * * ";;
+  *@WEEKLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * 0" && CRONS="0 0 * * 0";;
+  *@DAILY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * *" && CRONS="0 0 * * *";;
+  *@MIDNIGHT* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * *" && CRONS="0 0 * * *";;
+  *@HOURLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 * * * *" && CRONS="0 * * * *";;
+  *@* ) echo "WARNING: Cron shortcut $CRON is not supported. Stopping." && exit 1;;
+  * ) CRONS=$CRON;;
+esac
+
+if [ -z "$CRONS" ]
+then
+  echo "INFO: No CRON setting found. Stopping. (Tip: Add CRON=\"0 0 * * *\" to perform sync every midnight)"
   exit 1
 else
-  if [ -z "${SYNC_ON_STARTUP}" ]
+  # Setup cron schedule
+  crontab -d
+  echo "$CRONS su $USER -c /usr/bin/dijnet-bot-sync.sh >> /var/log/dijnet-bot/dijnet-bot-sync.crontab.log 2>&1" > /tmp/crontab.tmp
+  if [ -z "$CRON_ABORT" ]
   then
-    echo "INFO: Add SYNC_ON_STARTUP=true to perform a sync upon boot"
+    echo "INFO: Add CRON_ABORT=\"0 6 * * *\" to cancel outstanding sync at 6am"
   else
-    su "$USER" -c dijnet-bot
+    echo "$CRON_ABORT /usr/bin/dijnet-bot-sync-abort.sh >> /var/log/dijnet-bot/dijnet-bot-sync-abort.crontab.log 2>&1" >> /tmp/crontab.tmp
   fi
+  crontab /tmp/crontab.tmp
+  rm /tmp/crontab.tmp
 
-  # Re-write cron shortcut
-  case "$(echo "$CRON" | tr '[:lower:]' '[:upper:]')" in
-      *@YEARLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 1 *" && CRONS="0 0 1 1 *";;
-      *@ANNUALLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 1 *" && CRONS="0 0 1 1 *";;
-      *@MONTHLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 1 * *" && CRONS="0 0 1 * * ";;
-      *@WEEKLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * 0" && CRONS="0 0 * * 0";;
-      *@DAILY* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * *" && CRONS="0 0 * * *";;
-      *@MIDNIGHT* ) echo "INFO: Cron shortcut $CRON re-written to 0 0 * * *" && CRONS="0 0 * * *";;
-      *@HOURLY* ) echo "INFO: Cron shortcut $CRON re-written to 0 * * * *" && CRONS="0 * * * *";;
-      *@* ) echo "WARNING: Cron shortcut $CRON is not supported. Stopping." && exit 1;;
-      * ) CRONS=$CRON;;
-  esac
-
-  if [ -z "$CRONS" ]
-  then
-    echo "INFO: No CRON setting found. Stopping. (Tip: Add CRON=\"0 0 * * *\" to perform sync every midnight)"
-    exit 1
-  else
-    # Setup cron schedule
-    crontab -d
-    echo "$CRONS su $USER -c dijnet-bot >> /var/log/dinet-bot/dinet-bot.crontab.log 2>&1" > /tmp/crontab.tmp
-    if [ -z "$CRON_ABORT" ]
-    then
-      echo "INFO: Add CRON_ABORT=\"0 6 * * *\" to cancel outstanding sync at 6am"
-    else
-      echo "$CRON_ABORT dijnet-bot >> /var/log/dinet-bot/dinet-bot.crontab.log 2>&1" >> /tmp/crontab.tmp
-    fi
-    crontab /tmp/crontab.tmp
-    rm /tmp/crontab.tmp
-
-    # Start cron
-    echo "INFO: Starting crond ..."
-    touch /tmp/sync.log
-    touch /tmp/crond.log
-    crond -b -l 0 -L /tmp/crond.log
-    echo "INFO: crond started"
-    tail -F /tmp/crond.log /tmp/sync.log
-  fi
+  # Start cron
+  echo "INFO: Starting crond ..."
+  touch /tmp/sync.log
+  touch /tmp/crond.log
+  crond -b -l 0 -L /tmp/crond.log
+  echo "INFO: crond started"
+  tail -F /tmp/crond.log /tmp/sync.log
 fi
