2a3abd6bb3d7de46a782687ff78c962a90adbec3
root <l4t3b0@gmail.com>
2020-10-31 18:13:37 +0000

    initial release

--- root/usr/bin/dijnet-bot-sync.sh
+++ root/usr/bin/dijnet-bot-sync.sh
@@ -87,6 +87,7 @@ else
   echo $$ > ${pid_file}
   echo "INFO: PID file created successfuly: ${pid_file}"
 
+  echo "INFO: Reading config file: ${DIJNET_CONFIG_FILE}"
   xargs -a ${DIJNET_CONFIG_FILE} -r
 
   healthchecks_io_start
